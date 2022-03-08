6c0f622c00a2af1b31a41044f389c7b4435063bb
Carlos Lopez <calopezb@redhat.com>
2022-03-08 09:20:21 +0100

    Included rest call

--- src/main/java/com/clbartolome/route/controller/RoutingController.java
+++ src/main/java/com/clbartolome/route/controller/RoutingController.java
@@ -1,18 +1,18 @@
 package com.clbartolome.route.controller;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/route")
 public class RoutingController {
 
-  @Value("${app.routing.name}") 
+  @Value("${app.routing.name}")
   private String name;
 
   @Value("${app.routing.destinations:}")
@@ -22,13 +22,15 @@ public class RoutingController {
   }
 
   @GetMapping
-  public ResponseEntity<String> route() {    
-
+  public ResponseEntity<String> route() {
+    RestTemplate restTemplate = new RestTemplate();
+    String responses = "";
     for (String route : stringArrayWithDefaults) {
-      System.out.println(">>>" + route);
+      ResponseEntity<String> response = restTemplate.getForEntity(route, String.class);
+      responses = responses + "::" + response;
     }
 
-    return ResponseEntity.status(HttpStatus.OK).body(name);
+    return ResponseEntity.status(HttpStatus.OK).body(name + responses);
   }
 
 }
